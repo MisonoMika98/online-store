ee0a1ff837b3750a2d41649900213d7479dad79e
ariel raymundo <ariraymundo98@gmail.com>
2026-04-24 11:40:07 -0700

    added code

--- src/main/java/com/pluralsight/OnlineStoreApp.java
+++ src/main/java/com/pluralsight/OnlineStoreApp.java
@@ -1,8 +1,37 @@
 package com.pluralsight;
 
 
-public class OnlineStore {
-    static void main() {
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class OnlineStoreApp
+{
+    static Scanner userInput = new Scanner(System.in);
+    static ArrayList<Product> products;
+    static ArrayList<Product> shoppingCart;
+
+    static void main()
+    {
+        displayHomeScreen();
+    }
+
+
+    static void displayHomeScreen()
+    {
+        System.out.println();
+        System.out.println("Welcome to my top secret store...");
+    }
+
+    static ArrayList<Product> loadProducts()
+    {
+        // created new arraylist
+       ArrayList<Product> products = new ArrayList<>();
+
+       // call filereader and buffedreader
+
+       // create seperate array to read .csv
+
+
+       return products;
     }
 }
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
-public class Product {
+public class Product
+{
+    private String sku;
+    private String productName;
+    private double price;
+    private String department;
+
+
+    // constructor
+
+    public Product(String sku, String productName, double price, String department)
+    {
+        this.sku = sku;
+        this.productName = productName;
+        this.price = price;
+        this.department = department;
+    }
+
+    // getters
+    public String getSku() {
+        return sku;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
 }
